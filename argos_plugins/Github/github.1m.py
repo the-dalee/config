ed024dbb731680a5776a17578e1257d5cea64aeb
Damain Lippok <damian.lippok@zalando.de>
2019-11-18 09:53:55 +0100

    Add Argos plugin for GHE PR list

---
+++ argos_plugins/Github/github.1m.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+import json
+import os
+from pathlib import Path
+
+import requests
+
+GH_ICON = """iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJ
+bWFnZVJlYWR5ccllPAAAAyRpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdp
+bj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6
+eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuNS1jMDIxIDc5LjE1
+NDkxMSwgMjAxMy8xMC8yOS0xMTo0NzoxNiAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJo
+dHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlw
+dGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wTU09Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEu
+MC9tbS8iIHhtbG5zOnN0UmVmPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvc1R5cGUvUmVz
+b3VyY2VSZWYjIiB4bWxuczp4bXA9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC8iIHhtcE1N
+OkRvY3VtZW50SUQ9InhtcC5kaWQ6MzlFQkFERkU4NkJCMTFFM0FBNTJFRTMzNTJEMUJDNDYiIHht
+cE1NOkluc3RhbmNlSUQ9InhtcC5paWQ6MzlFQkFERkQ4NkJCMTFFM0FBNTJFRTMzNTJEMUJDNDYi
+IHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENTNiAoTWFjaW50b3NoKSI+IDx4bXBN
+TTpEZXJpdmVkRnJvbSBzdFJlZjppbnN0YW5jZUlEPSJ4bXAuaWlkOkU1MTc4QTJFOTlBMDExRTI5
+QTE1QkMxMDQ2QTg5MDREIiBzdFJlZjpkb2N1bWVudElEPSJ4bXAuZGlkOkU1MTc4QTJGOTlBMDEx
+RTI5QTE1QkMxMDQ2QTg5MDREIi8+IDwvcmRmOkRlc2NyaXB0aW9uPiA8L3JkZjpSREY+IDwveDp4
+bXBtZXRhPiA8P3hwYWNrZXQgZW5kPSJyIj8+Kk5lQwAABYxJREFUeNrkm29oVXUYx3+7bM3V1Fnb
+qlltrtXWtYRa1nqxooY5E7EhKWGuaTDBagol9SIMDCKICASj+cISw/DPi16ZBakrUBnoC7nNoTMW
+y6I1c+LmVq6t78N9jpyu555znt855+536IHPi939/jzP95zznN+/kzc1NaUitirwJJgPasF94DZQ
+DG7hMqNgBFwEZ5kU+AH0R+lcXgQCJMBT4EXwLKgM2N7P4FvwJegCk6YKUA5eB23grogu2C/gc7AN
+/GGKABTsZtAOZqjc2DjYAT5kUfSNBNCkAGwGo1PTZ6PsQ4FuHLp3QD3YDR5QZtgZsAac1ElYokcG
+bATHDApesS/kUwf7GEkOKAK7wAvKbNsPXgZjYQowG3wNnlDxsONgCbgchgAU/GHwiIqXUT5o8hLB
+KwfcDA7FMHgrUR/iGLQEoGTyBWhQ8bUGjiFPR4A3QIuKv7VwLKIcQMnue5Dv0fjT/IwtAM3g+RyM
+BmkU+BXf3qc5Rx3xqDPBE7LjfkaCheCcj1HYKYe6JeBt8GcEo75L3HaJQ7+nfNQ/x7H9p67TFX4L
+1Pi4EocdfhsGH4BPwVbwqu0xGwI/8vT2N/77Gv+vAJSCO3n6PJ//Vjz72w62cPtORnfAwx7+1nBs
+W93ugGow7vOKtPkYa9eDl0Clxji9kuvW+yjb5tPncY7xet3MhjoFt2RzgIlU2DQL/O6017W/Be4B
+awXJqMCgTH+ToOxajvWG1+AmYVBlBglQKrxwmzIFoB9XCzt91CABpL6sti62JcBiXtKS2GMGCSD1
+pZxjvi7AKmED9PraYJAAG2yvVL+2yi7AImHl90C3QQJ03/B+97ZF1lCYVlN6BBV/BffykNQkoyF4
+H5grqJOkO6BR2NF2A4O35gifCOs0JjTW9vYaPPPbJ11LJAFqBRVoDf68wQLQI3BBUL424XPiY1lv
+DOb/ZwRla0iAOYIKv8dAgEFB2VtJgJmCChMxEEAyHigmAQoFFWbFQIDZgrKF0p2hmTEQQOQjCTAm
+KD8vBgJUCcqOkQBXBBXosEORwcEXKdmBjCskwICgQr5h0+BMW6i8V7LtNkAC9As7WWqwAM8Jy/cn
+hBMhspVKvq2eC0uwbxLrSWhMa+dpdJQLW6mRpLtpOlyuMcL7CTwErhoSPG2ApjQEuD3BQ0fp0ZJq
+lT6pZYpt0wieYh60nuWDGp2+At4xIPgt7IvU0jHzBkFdgD27HWDGNGyGFHHfulaXuTN0IkBjZ8Ey
+kJeDwKmPFtAXwN8TTltjrVkKfwcawXJW3G3v8DTYCKoiCLwGvAl6QthpbnU6J5jP2f1uh1Wgxbbx
+wv0qvT/vtZRGA6wuzs50+Pkb8JdgQtPMq1VJld7bnxtSzhjgJD5hzwEW611OZK6xlSvzeYbAsl3C
+x4PK7ozodOl6t93hfJByqbzOVnYh+MdHhxfBLI1bnuoMhRx8imPMKgDR5LG/nrSVfddHpx8HeO4/
+ClmApsw+snXsdk7gYMat+r5Hp0sDCLAkxOA7nfrI1nGxx2tmQUb5x8FuzgvD4Dw4wNm2MIAA1SEF
+38cx+RaAeBCMZGlwb44GOyUhBD/CsTj24TatpddXq3L+RIVmXnE4QzjJMaSylvBxFdqzKHsVrDD8
+Dmj36sOvIx0unewHDRENg4MI0BH2FyP0RcZOlzW3Ib7VLvPqDK0z1PEq7bDmLVwCLgnr0AhvnUp/
+0eJp0k9m6HO4fUp2nGZODgUY5PzUJVlHkxg1TEfnjxqY8I6yb12SSjqLm7T9/Ax4TaW/+JxuIx86
+2KcL4toBk1QFT1omXZLRHQHaL3Npl/r8jH3QjiGsbJ3kGd/fDo6WBWi31KG9a9xXMgzfw35tVfCR
+9l52dk8Ibe7htnq57YowfY7i4+lYWUL9z+1fAQYACqstE4NCc18AAAAASUVORK5CYII=""".replace(
+    "\n", "")
+
+
+def shorten(s: str):
+    return s if len(s) <= 50 else s[:50] + "..."
+
+
+if __name__ == "__main__":
+    root_path = Path(os.path.dirname(__file__))
+    config_path = root_path / 'github.config.json'
+
+    with config_path.open() as config_file:
+        config = json.load(config_file)
+
+    login = config['login']
+    token = config['token']
+    organization = config['organization']
+    username = config['username']
+    host = config['github_host']
+
+    prs = requests.get(f"{host}/api/v3/search/issues?q=is%3Aopen+is%3Apr+org%3A{organization}+archived%3Afalse",
+                       auth=(login, token)).json()
+    pr_count = prs['total_count']
+
+    issues = requests.get(f"{host}/api/v3/search/issues?q=is%3Aopen+is%3Aissue+assignee%3A{username}+archived%3Afalse",
+                          auth=(login, token)).json()
+    issues_count = issues['total_count']
+
+    issues_count_string = f"(assigned issues: {issues_count})" if issues_count > 0 else ""
+
+    message = f'{pr_count} {issues_count_string}| image={GH_ICON} imageWidth=18'
+    print(message)
+    print("---")
+
+    if pr_count > 0:
+        print("<b>Open Pull Requests:</b>")
+        for pr in prs.get('items', []):
+            title = shorten(pr['title'])
+            user = pr['user']['login']
+            repository = pr['repository_url'].split('/')[-1]
+
+            pr_details_url = pr.get('pull_request', {}).get('url')
+            if pr_details_url:
+                pr_details = requests.get(pr_details_url,
+                                          auth=(login, token)).json()
+                statuses_url = pr_details['_links'].get('statuses', {}).get(
+                    'href')
+                statuses = requests.get(statuses_url,
+                                        auth=(login, token)).json()
+                status_icon = '❌' if any([True for s in statuses if
+                                          s['state'] == 'failure']) else '✔️'
+
+            print(f"{title} \r<small>{status_icon} <b>📦 {repository}</b> 👤 {user}</small>| href={pr['html_url']} ")
+
+        print("---")
+
+        print(f"🔍 All pull request of {organization} | href={host}/pulls?utf8=%E2%9C%93&q=is%3Aopen+is%3Apr+org%3A{organization}")
+        print("---")
+
+    if issues_count > 0:
+        print("<b>Issues assigned to me:</b>")
+
+        for pr in issues.get('items', []):
+            title = shorten(pr['title'])
+            user = pr['user']['login']
+            repository = pr['repository_url'].split('/')[-1]
+            print(f"{title} | href={pr['html_url']} ")
+
+        print("---")
+    for links in config.get("links", []):
+        print(f"{links['title']} | href={links['url']}")
